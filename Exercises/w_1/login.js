ac2ebf33ee45487b9da12af36b1cf84b61b207c3
Frederik Lind <fli@vla.dk>
2023-02-06 16:05:52 +0100

    adding loginscr

---
+++ Exercises/w_1/login.js
@@ -0,0 +1,11 @@
+/**
+ * Verifies user
+ * @param {*} firstname 
+ * @param {*} lastname 
+ * @param {*} password 
+ */
+function login(firstname, lastname, password){
+    if (username === "frederik" && lastname === "lind" && password === "dude")
+        return true;
+    else return false;
+}
